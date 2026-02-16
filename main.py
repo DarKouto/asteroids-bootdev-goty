1148d6f7486b3e9dd81a4efc34536be1ccfcc25d
DarKouto <danielcouto9@live.com>
2026-02-16 01:30:51 +0000

    ch3 l2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 from logger import log_state
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
+from player import Player
 
 def main():
-
     # TERMINAL MESSAGES TO CHECK IF EVERYTHING IS OK
     print("Starting Asteroids with pygame version: 2.6.1!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,6 +18,11 @@ def main():
     # FPS / DELTA TIME
     clock = pygame.time.Clock()
     dt = 0
+
+    # PLAYER
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player_obj = Player(x,y)
     
     # GAME LOOP
     while True:
@@ -25,9 +30,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        # Clean Screen
         screen.fill("black")
+        # Draw Player
+        player_obj.draw(screen)
+        # Show canvas to user
         pygame.display.flip()
+        # Control frame rate
         dt = clock.tick(60)/1000
-    
+
 if __name__ == "__main__":
     main()
