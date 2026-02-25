f65fa8996639c8874b33cc4a9bf11025aced5ba7
DarKouto <danielcouto9@live.com>
2026-02-25 19:32:05 +0000

    new refactor main.py

--- main.py
+++ main.py
@@ -59,14 +59,13 @@ def main():
                     log_event("asteroid_shot")
                     asteroid.kill()
 
-        # Clean Screen
+        # CLEAN SCREEN / DRAW PLAYER
         screen.fill("black")
-        # Draw Player
         for item in drawable:
             item.draw(screen)
-        # Refresh the screen
+
+        # REFRESH THE SCREEN / SET FRAME RATE
         pygame.display.flip()
-        # Control frame rate
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
